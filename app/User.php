ae461dddcee5e5c5248b6cae9eed906b0a8e007d
peter <ptrmutie@gmail.com>
2018-12-02 03:03:02 +0300

    add author info and questions created date in questions

--- app/User.php
+++ app/User.php
@@ -31,4 +31,10 @@ class User extends Authenticatable
     public function questions(){
         return $this->hasMany(Question::class);
     }
+
+    public function getUrlAttribute()
+    {
+        // return route("questions.shoe", $this->id);
+        return '#';
+    }
 }
